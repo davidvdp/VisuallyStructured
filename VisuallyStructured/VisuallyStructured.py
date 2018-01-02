ae3c35cbec6519eb25d85ecc03f997e2ddfd5da6
David van der Pol <d.v.d.pol@qipc.com>
2018-01-02 12:25:50 +0100

    + Normalization Filter Filters/Normalize.py + HeatMap Filter Filters/HeatMap.py

--- VisuallyStructured/VisuallyStructured.py
+++ VisuallyStructured/VisuallyStructured.py
@@ -4,8 +4,7 @@ from time import localtime, strftime
 import os
 from StandardSettings import Settings
 
-from Filters import Sobel
-from Filters import Blur
+from Filters import Blur, Sobel, Normalize, HeatMap
 from Grabbers import File
 
 #TODO: autosave for properties
